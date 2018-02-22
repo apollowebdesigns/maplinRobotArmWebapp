98e9eb995780aab0b73bef164fe1526628ba2cf4
Andy Evans <andrewevans0@outlook.com>
2018-02-22 20:50:17 +0000

    more controls added to the python file

--- movearm.py
+++ movearm.py
@@ -6,3 +6,27 @@ def baseclockwise():
 def baseanticlockwise():
     MoveArm(1,[0,2,0]) #Rotate base clockwise
     return 'the arm has moved!!!'
+
+def up():
+    MoveArm(1,[64,0,0]) #Shoulder up
+    return 'the arm has moved!!!'
+
+def down():
+    MoveArm(1,[128,0,0]) #Shoulder down
+    return 'the arm has moved!!!'
+
+def elbowup():
+    MoveArm(1,[16,0,0]) #Elbow up
+    return 'the arm has moved!!!'
+
+def elbowdown():
+    MoveArm(1,[32,0,0]) #Elbow down
+    return 'the arm has moved!!!'
+
+def wristup():
+    MoveArm(1,[4,0,0]) #Wrist up
+    return 'the arm has moved!!!'
+
+def wristdown():
+    MoveArm(1,[8,0,0]) # Wrist down
+    return 'the arm has moved!!!'
