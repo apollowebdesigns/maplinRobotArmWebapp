4d34b0e580b120cf159c32f2ea4052e03fa6f7ff
Andy Evans <andrewevans0@outlook.com>
2018-02-22 19:48:09 +0000

    added home url

--- maplinRobotArmWebapp/urls.py
+++ maplinRobotArmWebapp/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.urls import include, path
 from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+    path('home/', include('home.urls')),
+    path('arm/', include('arm.urls')),
     url(r'^admin/', admin.site.urls),
 ]
