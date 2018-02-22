ebcafdcc5791d945c27cbeee27856e5bd21dc456
Andy Evans <andrewevans0@outlook.com>
2018-02-22 20:53:45 +0000

    added to the routes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from movearm import baseclockwise, baseanticlockwise
+from movearm import baseclockwise, baseanticlockwise, up, down, elbowup, elbowdown, wristup, wristdown, gripopen, gripclose, lighton, lightoff
 app = Flask(__name__)
 
 @app.route('/')
