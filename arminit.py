a9b806be23c9c4676559020243526d54f17f1e62
Andy Evans <andrewevans0@outlook.com>
2018-02-22 20:26:26 +0000

    adding arm files

---
+++ arminit.py
@@ -0,0 +1,20 @@
+#import the USB and Time librarys into Python
+import usb.core, usb.util, time
+ 
+#Allocate the name 'RoboArm' to the USB device
+RoboArm = usb.core.find(idVendor=0x1267, idProduct=0x1)
+ 
+#Check if the arm is detected and warn if not
+if RoboArm is None:
+    raise ValueError("Arm not found")
+ 
+#Create a variable for duration
+Duration=1
+ 
+#Define a procedure to execute each movement
+def MoveArm(Duration, ArmCmd):
+    #Start the movement
+    RoboArm.ctrl_transfer(0x40,6,0x100,0,ArmCmd,3)
+    #Stop the movement after waiting a specified duration
+    time.sleep(Duration)
+    ArmCmd=[0,0,0]
