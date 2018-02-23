06bed91beabdb0a3d020c023b8cd0ce72ced36e8
Andy Evans <andrewevans0@outlook.com>
2018-02-23 19:07:28 +0000

    name changing

--- maplinRobotArm/settings.py
+++ maplinRobotArm/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'moveArm.apps.MovearmConfig',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- maplinRobotArm/urls.py
+++ maplinRobotArm/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('moveArm/', include('moveArm.urls')),
+    path('arm/', include('arm.urls')),
     url(r'^admin/', admin.site.urls),
 ]
