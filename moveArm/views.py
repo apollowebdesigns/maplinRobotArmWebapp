9e45db079dd8ba8ca0b756bba30ef28d8f0e53eb
Andy Evans <andrewevans0@outlook.com>
2018-02-23 19:00:43 +0000

    still no templates :(

--- moveArm/views.py
+++ moveArm/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+    context = {}
     return render(request, 'moveArm/index.html', context)
