cbfddd17bf9b62ff9dc0e8fb68abf6ad2d1fea7a
Andy Evans <andrewevans0@outlook.com>
2018-02-23 18:59:35 +0000

    adding the html

--- moveArm/views.py
+++ moveArm/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse({'body': 'hello again world'})
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'moveArm/index.html', context)
