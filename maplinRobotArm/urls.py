87a8043bb4b5eaae4a303bbb1469e88936f8c0f2
Andy Evans <andrewevans0@outlook.com>
2018-02-23 18:53:56 +0000

    returns data

--- maplinRobotArm/urls.py
+++ maplinRobotArm/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path('moveArm/', include('moveArm.urls')),
