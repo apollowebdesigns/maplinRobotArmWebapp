b0d9b2ca6c6c0e7e14166a6b9fe473f2b68915a9
Andy Evans <andrewevans0@outlook.com>
2018-02-26 17:40:13 +0000

    added second url route to the index

--- arm/views.py
+++ arm/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     context = {}
     return render(request, 'arm/index.html', context)
+
+def test(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
