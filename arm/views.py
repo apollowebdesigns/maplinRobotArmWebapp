91851398412adb438c72c05a71401270b88715c1
Andy Evans <andrewevans0@outlook.com>
2018-02-26 18:04:03 +0000

    added more

--- arm/views.py
+++ arm/views.py
@@ -8,48 +8,48 @@ def index(request):
 
 def baseclockwise(request):
     MoveArm(1,[0,1,0]) #Rotate base anti-clockwise
-    return 'the arm has moved!!!'
+    return HttpResponse('the arm has moved!!!')
 
 def baseanticlockwise(request):
     MoveArm(1,[0,2,0]) #Rotate base clockwise
-    return 'the arm has moved!!!'
+    return HttpResponse('the arm has moved!!!')
 
 def up(request):
     MoveArm(1,[64,0,0]) #Shoulder up
-    return 'the arm has moved!!!'
+    return HttpResponse('the arm has moved!!!')
 
 def down(request):
     MoveArm(1,[128,0,0]) #Shoulder down
-    return 'the arm has moved!!!'
+    return HttpResponse('the arm has moved!!!')
 
 def elbowup(request):
     MoveArm(1,[16,0,0]) #Elbow up
-    return 'the arm has moved!!!'
+    return HttpResponse('the arm has moved!!!')
 
 def elbowdown(request):
     MoveArm(1,[32,0,0]) #Elbow down
-    return 'the arm has moved!!!'
+    return HttpResponse('the arm has moved!!!')
 
 def wristup(request):
     MoveArm(1,[4,0,0]) #Wrist up
-    return 'the arm has moved!!!'
+    return HttpResponse('the arm has moved!!!')
 
 def wristdown(request):
     MoveArm(1,[8,0,0]) # Wrist down
-    return 'the arm has moved!!!'
+    return HttpResponse('the arm has moved!!!')
 
 def gripopen(request):
     MoveArm(1,[2,0,0]) #Grip open
-    return 'the arm has moved!!!'
+    return HttpResponse('the arm has moved!!!')
 
 def gripclose(request):
     MoveArm(1,[1,0,0]) #Grip close
-    return 'the arm has moved!!!'
+    return HttpResponse('the arm has moved!!!')
 
 def lighton(request):
     MoveArm(1,[0,0,1]) #Light on
-    return 'the arm has moved!!!'
+    return HttpResponse('the arm has moved!!!')
 
 def lightoff(request):
     MoveArm(1,[0,0,0]) #Light off
-    return 'the arm has moved!!!'
+    return HttpResponse('the arm has moved!!!')
