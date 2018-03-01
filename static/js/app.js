94b9c280ea9cecea0dbd8de4b80661c598488081
Andy Evans <andrewevans0@outlook.com>
2018-03-01 21:33:00 +0000

    added in new stuff

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,16 @@
-function test() {
-    console.log('this function has been hit');
-    document.getElementById('test').innerHTML = 'another test???';
+// function test() {
+//     console.log('this function has been hit');
+//     document.getElementById('test').innerHTML = 'another test???';
+// }
+
+angular
+    .module('app', []);
+
+angular
+    .module('app')
+    .controller('controllerT', controllerT)
+
+function controllerT() {
+    var vm = this;
+    vm.test = 'second';
 }
