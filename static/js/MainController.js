8cd0b9cc86ff4c1b28af23c41f8adda82a17525c
Andy Evans <andrewevans0@outlook.com>
2018-03-01 21:39:05 +0000

    moved to single file due to weird django quirk

--- static/js/MainController.js
+++
@@ -1,8 +0,0 @@
-angular
-    .module('app')
-    .controller('MainController', MainController)
-
-function MainController() {
-    var vm = this;
-    vm.test = 'from main controller';
-}
--- static/js/app.js
+++ static/js/app.js
@@ -5,3 +5,12 @@
 
 angular
     .module('app', []);
+
+angular
+    .module('app')
+    .controller('MainController', MainController)
+
+function MainController() {
+    var vm = this;
+    vm.test = 'from main controller';
+}
