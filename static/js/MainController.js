0d16fe487ec9941b6b9ce09a09ee4a236ee2d3d6
Andy Evans <andrewevans0@outlook.com>
2018-03-03 22:24:14 +0000

    added in urls

--- static/js/MainController.js
+++ static/js/MainController.js
@@ -2,10 +2,15 @@ angular
     .module('app')
     .controller('MainController', MainController)
 
-function MainController() {
+MainController.$inject = ['$http', '$log'];
+
+function MainController($http, $log) {
     var vm = this;
     vm.test = 'from main controller';
-    vm.moveArm = function() {
-        console.log('I have been hit!!!');
+    vm.moveArm = (url) => {
+        $log.info('I have been hit!!!');
+        $http
+            .get("/arm/" + url)
+            .then(response => console.log(response));
     }
 }
