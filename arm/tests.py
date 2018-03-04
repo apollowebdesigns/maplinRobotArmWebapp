29fc238e662638f399b7c58c20697c9b7e18b67b
Andy Evans <andrewevans0@outlook.com>
2018-03-04 12:07:01 +0000

    added in basic tests for python

--- arm/tests.py
+++ arm/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from unittest.mock import MagicMock
+from unittest.mock import patch
 
-# Create your tests here.
+def stub():
+    return 'hello'
+
+class ArmTestCase(TestCase):
+    def setUp(self):
+
+    def test_animals_can_speak(self):
+        self.assertEqual(stub(), 'The lion says "roar"')
