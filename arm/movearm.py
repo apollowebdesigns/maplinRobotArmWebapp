df99ead1daf764a8a1db6de1bbb2a3134e844095
Andy Evans <andrewevans0@outlook.com>
2018-03-06 19:21:04 +0000

    changed vars

--- arm/movearm.py
+++ arm/movearm.py
@@ -1,52 +1,53 @@
 """
 Times are controlled from within this file for grip
 """
+duration = 0.2
 
 from arminit import MoveArm
 def baseclockwise():
-    MoveArm(1,[0,1,0]) #Rotate base anti-clockwise
+    MoveArm(duration,[0,1,0]) #Rotate base anti-clockwise
     return 'the arm has moved!!!'
 
 def baseanticlockwise():
-    MoveArm(1,[0,2,0]) #Rotate base clockwise
+    MoveArm(duration,[0,2,0]) #Rotate base clockwise
     return 'the arm has moved!!!'
 
 def up():
-    MoveArm(1,[64,0,0]) #Shoulder up
+    MoveArm(duration,[64,0,0]) #Shoulder up
     return 'the arm has moved!!!'
 
 def down():
-    MoveArm(1,[128,0,0]) #Shoulder down
+    MoveArm(duration,[128,0,0]) #Shoulder down
     return 'the arm has moved!!!'
 
 def elbowup():
-    MoveArm(1,[16,0,0]) #Elbow up
+    MoveArm(duration,[16,0,0]) #Elbow up
     return 'the arm has moved!!!'
 
 def elbowdown():
-    MoveArm(1,[32,0,0]) #Elbow down
+    MoveArm(duration,[32,0,0]) #Elbow down
     return 'the arm has moved!!!'
 
 def wristup():
-    MoveArm(1,[4,0,0]) #Wrist up
+    MoveArm(duration,[4,0,0]) #Wrist up
     return 'the arm has moved!!!'
 
 def wristdown():
-    MoveArm(1,[8,0,0]) # Wrist down
+    MoveArm(duration,[8,0,0]) # Wrist down
     return 'the arm has moved!!!'
 
 def gripopen():
-    MoveArm(0.1,[2,0,0]) #Grip open
+    MoveArm(duration,[2,0,0]) #Grip open
     return 'the arm has moved!!!'
 
 def gripclose():
-    MoveArm(0.1,[1,0,0]) #Grip close
+    MoveArm(duration,[1,0,0]) #Grip close
     return 'the arm has moved!!!'
 
 def lighton():
-    MoveArm(1,[0,0,1]) #Light on
+    MoveArm(duration,[0,0,1]) #Light on
     return 'the arm has moved!!!'
 
 def lightoff():
-    MoveArm(1,[0,0,0]) #Light off
+    MoveArm(duration,[0,0,0]) #Light off
     return 'the arm has moved!!!'
