c2366a9a06f8a68cd67206daf7b36953262f3726
Andy Evans <andrewevans0@outlook.com>
2018-05-03 20:10:22 +0100

    changed click to mousedown

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,2 @@
-// function test() {
-//     console.log('this function has been hit');
-//     document.getElementById('test').innerHTML = 'another test???';
-// }
-
 angular
     .module('app', []);
-
-// angular
-//     .module('app')
-//     .controller('MainController', MainController)
-
-// function MainController() {
-//     var vm = this;
-//     vm.test = 'from main controller';
-// }
