0ca94e984561e232baa8050ee3ad87fe865c1a73
Andy Evans <andrewevans0@outlook.com>
2018-05-03 20:20:27 +0100

    testing

--- arm/arminit.py
+++ arm/arminit.py
@@ -1,28 +1,61 @@
-#import the USB and Time librarys into Python
-import usb.core, usb.util, time
- 
-#Allocate the name 'RoboArm' to the USB device
-RoboArm = usb.core.find(idVendor=0x1267, idProduct=0x1)
- 
-#Check if the arm is detected and warn if not
-if RoboArm is None:
-    raise ValueError("Arm not found")
- 
-#Create a variable for duration
-Duration=0.5
- 
-#Define a procedure to execute each movement
-def MoveArm(Duration, ArmCmd):
-    #Start the movement
-    RoboArm.ctrl_transfer(0x40,6,0x100,0,ArmCmd,3)
-    #Stop the movement after waiting a specified duration
-    time.sleep(Duration)
-    ArmCmd=[0,0,0]
-    RoboArm.ctrl_transfer(0x40,6,0x100,0,ArmCmd,3)
+from arminit import MoveArm, StartArm, StopArm
 
-def StartArm(ArmCmd):
-    RoboArm.ctrl_transfer(0x40,6,0x100,0,ArmCmd,3)
+def start_baseclockwise():
+    StartArm([0,1,0]) #Rotate base anti-clockwise
+    return 'the arm has moved!!!'
 
-def StopArm():
-    ArmCmd=[0,0,0]
-    RoboArm.ctrl_transfer(0x40,6,0x100,0,ArmCmd,3)
+def start_baseanticlockwise():
+    StartArm([0,2,0]) #Rotate base anti-clockwise
+    return 'the arm has moved!!!'
+
+def stop():
+    StopArm() #Rotate base anti-clockwise
+    return 'the arm has moved!!!'
+
+def baseclockwise():
+    StartArm([0,1,0]) #Rotate base anti-clockwise
+    return 'the arm has moved!!!'
+
+def baseanticlockwise():
+    StartArm([0,2,0]) #Rotate base clockwise
+    return 'the arm has moved!!!'
+
+def up():
+    StartArm([64,0,0]) #Shoulder up
+    return 'the arm has moved!!!'
+
+def down():
+    StartArm([128,0,0]) #Shoulder down
+    return 'the arm has moved!!!'
+
+def elbowup():
+    StartArm([16,0,0]) #Elbow up
+    return 'the arm has moved!!!'
+
+def elbowdown():
+    StartArm([32,0,0]) #Elbow down
+    return 'the arm has moved!!!'
+
+def wristup():
+    StartArm([4,0,0]) #Wrist up
+    return 'the arm has moved!!!'
+
+def wristdown():
+    StartArm([8,0,0]) # Wrist down
+    return 'the arm has moved!!!'
+
+def gripopen():
+    StartArm([2,0,0]) #Grip open
+    return 'the arm has moved!!!'
+
+def gripclose():
+    StartArm([1,0,0]) #Grip close
+    return 'the arm has moved!!!'
+
+def lighton():
+    StartArm([0,0,1]) #Light on
+    return 'the arm has moved!!!'
+
+def lightoff():
+    StartArm([0,0,0]) #Light off
+    return 'the arm has moved!!!'
