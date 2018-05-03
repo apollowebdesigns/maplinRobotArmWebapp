315598da2a3ea94564131f625e868dde7d236b0b
Andy Evans <andrewevans0@outlook.com>
2018-05-03 20:56:00 +0100

    tesing

--- static/js/MainController.js
+++ static/js/MainController.js
@@ -1,6 +1,25 @@
 angular
     .module('app')
     .controller('MainController', MainController)
+    .directive('myTouchstart', [function() {
+        return function(scope, element, attr) {
+
+            element.on('touchstart', function(event) {
+                scope.$apply(function() { 
+                    scope.$eval(attr.myTouchstart); 
+                });
+            });
+        };
+    }]).directive('myTouchend', [function() {
+        return function(scope, element, attr) {
+
+            element.on('touchend', function(event) {
+                scope.$apply(function() { 
+                    scope.$eval(attr.myTouchend); 
+                });
+            });
+        };
+    }]);
 
 MainController.$inject = ['$http', '$log'];
 
