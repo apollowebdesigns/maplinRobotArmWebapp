88ebc35db08acea0bd0852c9603e1d632589e288
Andy Evans <andrewevans0@outlook.com>
2018-05-03 20:40:38 +0100

    updated

--- arm/views.py
+++ arm/views.py
@@ -6,54 +6,54 @@ def index(request):
     context = {}
     return render(request, 'arm/index.html', context)
 
-def stop():
+def stop(request):
     StopArm() #Rotate base anti-clockwise
     return HttpResponse('the arm has moved!!!')
 
-def left():
+def left(request):
     StartArm([0,1,0]) #Rotate base anti-clockwise
     return HttpResponse('the arm has moved!!!')
-    
-def right():
+
+def right(request):
     StartArm([0,2,0]) #Rotate base clockwise
     return HttpResponse('the arm has moved!!!')
 
-def up():
+def up(request):
     StartArm([64,0,0]) #Shoulder up
     return HttpResponse('the arm has moved!!!')
 
-def down():
+def down(request):
     StartArm([128,0,0]) #Shoulder down
     return HttpResponse('the arm has moved!!!')
 
-def elbowup():
+def elbowup(request):
     StartArm([16,0,0]) #Elbow up
     return HttpResponse('the arm has moved!!!')
 
-def elbowdown():
+def elbowdown(request):
     StartArm([32,0,0]) #Elbow down
     return HttpResponse('the arm has moved!!!')
 
-def wristup():
+def wristup(request):
     StartArm([4,0,0]) #Wrist up
     return HttpResponse('the arm has moved!!!')
 
-def wristdown():
+def wristdown(request):
     StartArm([8,0,0]) # Wrist down
     return HttpResponse('the arm has moved!!!')
 
-def gripopen():
+def gripopen(request):
     StartArm([2,0,0]) #Grip open
     return HttpResponse('the arm has moved!!!')
 
-def gripclose():
+def gripclose(request):
     StartArm([1,0,0]) #Grip close
     return HttpResponse('the arm has moved!!!')
 
-def lighton():
+def lighton(request):
     StartArm([0,0,1]) #Light on
     return HttpResponse('the arm has moved!!!')
 
-def lightoff():
+def lightoff(request):
     StartArm([0,0,0]) #Light off
     return HttpResponse('the arm has moved!!!')
