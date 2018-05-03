48656f817028e862d2fa88be5d7cf3c115ce9160
Andy Evans <andrewevans0@outlook.com>
2018-05-03 20:24:09 +0100

    updated the views file

--- arm/views.py
+++ arm/views.py
@@ -1,55 +1,67 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from arminit import MoveArm
+from arminit import MoveArm, StartArm, StopArm
 
 def index(request):
     context = {}
     return render(request, 'arm/index.html', context)
 
-def left(request):
-    MoveArm(1,[0,2,0]) #Rotate base clockwise
-    return HttpResponse('the arm has moved!!!')
+def start_baseclockwise():
+    StartArm([0,1,0]) #Rotate base anti-clockwise
+    return 'the arm has moved!!!'
 
-def right(request):
-    MoveArm(1,[0,1,0]) #Rotate base anti-clockwise
-    return HttpResponse('the arm has moved!!!')
+def start_baseanticlockwise():
+    StartArm([0,2,0]) #Rotate base anti-clockwise
+    return 'the arm has moved!!!'
 
-def up(request):
-    MoveArm(1,[64,0,0]) #Shoulder up
-    return HttpResponse('the arm has moved!!!')
+def stop():
+    StopArm() #Rotate base anti-clockwise
+    return 'the arm has moved!!!'
 
-def down(request):
-    MoveArm(1,[128,0,0]) #Shoulder down
-    return HttpResponse('the arm has moved!!!')
+def baseclockwise():
+    StartArm([0,1,0]) #Rotate base anti-clockwise
+    return 'the arm has moved!!!'
 
-def elbowup(request):
-    MoveArm(1,[16,0,0]) #Elbow up
-    return HttpResponse('the arm has moved!!!')
+def baseanticlockwise():
+    StartArm([0,2,0]) #Rotate base clockwise
+    return 'the arm has moved!!!'
 
-def elbowdown(request):
-    MoveArm(1,[32,0,0]) #Elbow down
-    return HttpResponse('the arm has moved!!!')
+def up():
+    StartArm([64,0,0]) #Shoulder up
+    return 'the arm has moved!!!'
 
-def wristup(request):
-    MoveArm(1,[4,0,0]) #Wrist up
-    return HttpResponse('the arm has moved!!!')
+def down():
+    StartArm([128,0,0]) #Shoulder down
+    return 'the arm has moved!!!'
 
-def wristdown(request):
-    MoveArm(1,[8,0,0]) # Wrist down
-    return HttpResponse('the arm has moved!!!')
+def elbowup():
+    StartArm([16,0,0]) #Elbow up
+    return 'the arm has moved!!!'
 
-def gripopen(request):
-    MoveArm(1,[2,0,0]) #Grip open
-    return HttpResponse('the arm has moved!!!')
+def elbowdown():
+    StartArm([32,0,0]) #Elbow down
+    return 'the arm has moved!!!'
 
-def gripclose(request):
-    MoveArm(1,[1,0,0]) #Grip close
-    return HttpResponse('the arm has moved!!!')
+def wristup():
+    StartArm([4,0,0]) #Wrist up
+    return 'the arm has moved!!!'
 
-def lighton(request):
-    MoveArm(1,[0,0,1]) #Light on
-    return HttpResponse('the arm has moved!!!')
+def wristdown():
+    StartArm([8,0,0]) # Wrist down
+    return 'the arm has moved!!!'
 
-def lightoff(request):
-    MoveArm(1,[0,0,0]) #Light off
-    return HttpResponse('the arm has moved!!!')
+def gripopen():
+    StartArm([2,0,0]) #Grip open
+    return 'the arm has moved!!!'
+
+def gripclose():
+    StartArm([1,0,0]) #Grip close
+    return 'the arm has moved!!!'
+
+def lighton():
+    StartArm([0,0,1]) #Light on
+    return 'the arm has moved!!!'
+
+def lightoff():
+    StartArm([0,0,0]) #Light off
+    return 'the arm has moved!!!'
