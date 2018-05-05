73af2898b8474be4cd1ba70874218bd5171be697
Andy Evans <andrewevans0@outlook.com>
2018-05-05 13:50:43 +0100

    updated features to have selenium

--- features/example.feature
+++ features/example.feature
@@ -1,5 +1,9 @@
 # -- FILE: features/example.feature
 Feature: Showing off behave
+  Scenario: Testing selenium
+    Given I fire up a web browser
+     When I hit the keys
+     Then its all ok
 
   Scenario: Run a simple test
     Given we have behave installed
--- features/steps/example_steps.py
+++ features/steps/example_steps.py
@@ -1,5 +1,25 @@
 # -- FILE: features/steps/example_steps.py
 from behave import given, when, then, step
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Chrome()
+
+@given('I fire up a web browser')
+def step_impl(context):
+    driver.get("http://www.python.org")
+    assert "Python" in driver.title
+
+@when('I hit the keys')
+def step_impl(context):  # -- NOTE: number is converted into integer
+    elem = driver.find_element_by_name("q")
+    elem.clear()
+    elem.send_keys("pycon")
+    elem.send_keys(Keys.RETURN)
+    assert "No results found." not in driver.page_source
+
+@then('its all ok')
+def step_impl(context):
+    driver.close()
 
 @given('we have behave installed')
 def step_impl(context):
